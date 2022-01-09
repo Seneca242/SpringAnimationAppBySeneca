7f40a20c69e30bcb8af59c5e235e214868d911ec
Seneca <flabbergasted242@gmail.com>
2022-01-09 15:44:40 +0300

    Initial Commit

---
+++ SpringAnimationAppBySeneca/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAnimationAppBySeneca
+//
+//  Created by Дмитрий Дмитрий on 09.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
